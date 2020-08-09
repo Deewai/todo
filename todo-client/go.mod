cb3e164306a1101da3aa77a2035232d99845adba
Abdullahi Adedayo <deewai48@gmail.com>
2020-08-09 20:54:33 +0100

    added tests

--- todo-client/go.mod
+++ todo-client/go.mod
@@ -4,8 +4,9 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.3
-	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/gorilla/mux v1.7.4
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/stretchr/testify v1.4.0
 	go.uber.org/zap v1.15.0
 	google.golang.org/grpc v1.31.0
 )
--- todo-service/pkg/todo/todo_test.go
+++ todo-service/pkg/todo/todo_test.go
@@ -9,38 +9,38 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type MyRepo struct{
+type myRepo struct{
 	err bool
 }
 
-func (r *MyRepo) AddItem(pk string, item interface{}) error{
+func (r *myRepo) AddItem(pk string, item interface{}) error{
 	if r.err{
 		return errors.New("A sample error")
 	}
 	return nil
 }
 
-func (r *MyRepo) DeleteItem(pk string) error{
+func (r *myRepo) DeleteItem(pk string) error{
 	if r.err{
 		return errors.New("A sample error")
 	}
 	return nil
 }
 
-func (r *MyRepo) GetItems() ([]interface{}, error){
+func (r *myRepo) GetItems() ([]interface{}, error){
 	if r.err{
 		return nil, errors.New("A sample error")
 	}
 	return []interface{}{}, nil
 }
 func TestNewService(t *testing.T){
-	repo := &MyRepo{}
+	repo := &myRepo{}
 	expected := &Service{repo}
 	assert.EqualValues(t, expected, NewService(repo))
 }
 
 func TestAddTodoSuccess(t *testing.T){
-	repo := &MyRepo{}
+	repo := &myRepo{}
 	service := &Service{repo}
 	req := todo.Todo{}
 	got, err := service.AddTodo(context.Background(), &req)
@@ -50,7 +50,7 @@ func TestAddTodoSuccess(t *testing.T){
 }
 
 func TestAddTodoError(t *testing.T){
-	repo := &MyRepo{err: true}
+	repo := &myRepo{err: true}
 	service := &Service{repo}
 	req := todo.Todo{}
 	got, err := service.AddTodo(context.Background(), &req)
@@ -59,7 +59,7 @@ func TestAddTodoError(t *testing.T){
 }
 
 func TestGetTodosSuccess(t *testing.T){
-	repo := &MyRepo{}
+	repo := &myRepo{}
 	service := &Service{repo}
 	req := todo.GetRequest{}
 	got, err := service.GetTodos(context.Background(), &req)
@@ -68,7 +68,7 @@ func TestGetTodosSuccess(t *testing.T){
 }
 
 func TestGetTodosError(t *testing.T){
-	repo := &MyRepo{err: true}
+	repo := &myRepo{err: true}
 	service := &Service{repo}
 	req := todo.GetRequest{}
 	got, err := service.GetTodos(context.Background(), &req)
@@ -77,7 +77,7 @@ func TestGetTodosError(t *testing.T){
 }
 
 func TestDeleteTodoSuccess(t *testing.T){
-	repo := &MyRepo{}
+	repo := &myRepo{}
 	service := &Service{repo}
 	req := todo.DeleteRequest{Id:""}
 	got, err := service.DeleteTodo(context.Background(), &req)
@@ -86,7 +86,7 @@ func TestDeleteTodoSuccess(t *testing.T){
 }
 
 func TestDeleteTodoError(t *testing.T){
-	repo := &MyRepo{err: true}
+	repo := &myRepo{err: true}
 	service := &Service{repo}
 	req := todo.DeleteRequest{Id:""}
 	got, err := service.DeleteTodo(context.Background(), &req)
